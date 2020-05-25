f5fd9e870272c689459235ca4066d62c46250730
Steve Skladany <steve@skladany.net>
2020-05-25 13:36:32 -0400

    Bug fix

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.get("/strava/distance_run/", async (req, res) => {
 
   // 1/1/2020
   const after = req.query.after || 1577836800;
-  const after = req.query.per_page || 30;
+  const per_page = req.query.per_page || 30;
   let page = req.query.page || 1;
   const endpoint = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&after=${after}&per_page=${per_page}&page=${page}&`;
 

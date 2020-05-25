5bc7ec78bc47d6e3bb203817e43cd952c0bb89f6
Steve Skladany <steve@skladany.net>
2020-05-25 13:32:11 -0400

    Allow per_page to be controlled from front-end code

--- index.js
+++ index.js
@@ -38,8 +38,9 @@ app.get("/strava/distance_run/", async (req, res) => {
 
   // 1/1/2020
   const after = req.query.after || 1577836800;
+  const after = req.query.per_page || 30;
   let page = req.query.page || 1;
-  const endpoint = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&after=${after}&per_page=30&page=${page}&`;
+  const endpoint = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&after=${after}&per_page=${per_page}&page=${page}&`;
 
   try {
     let { data } = await axios.get(endpoint);

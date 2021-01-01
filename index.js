2d2426c288684e6c9f973730dee08802f3392a3c
Steve Skladany <steve@skladany.net>
2021-01-01 15:00:35 -0500

    2021

--- index.js
+++ index.js
@@ -37,8 +37,8 @@ app.get("/strava/distance_run/", async (req, res) => {
 
   let stats = [];
 
-  // 1/1/2020
-  const after = req.query.after || 1577836800;
+  // 1/1/2021
+  const after = req.query.after || 1609459200;
   const per_page = req.query.per_page || 30;
   let page = req.query.page || 1;
   const endpoint = `https://www.strava.com/api/v3/athlete/activities?access_token=${accessToken}&after=${after}&per_page=${per_page}&page=${page}&`;
